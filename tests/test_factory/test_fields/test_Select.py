ad28824af5b75149c49dd08c61c51a23cd886bed
argmaster <argmaster.wordl@gmail.com>
2021-09-20 01:04:55 +0200

    Added Select field and tests

---
+++ tests/test_factory/test_fields/test_Select.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from PyR3.factory.fields.Select import Select
+from unittest import TestCase, main
+
+
+class TestSelectField(TestCase):
+
+    def test_digest(self):
+        self.assertEqual(Select("ONE", 'TWO', "THREE").digest("TWO"), "TWO")
+
+    def test_digest_default(self):
+        self.assertEqual(Select("ONE", 'TWO', "THREE", default_index=0).digest(), "ONE")
+
+    def test_digest_key_error(self):
+        self.assertRaises(KeyError, lambda: Select("ONE", 'TWO', "THREE").digest())
+
+    def test_digest_key_error(self):
+        self.assertRaises(ValueError, lambda: Select("ONE", 'TWO', "THREE").digest("STH ELSE"))
+
+
+
+if __name__ == '__main__':
+    main()
