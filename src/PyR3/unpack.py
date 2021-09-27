8f2a86ffba7ff2bd84ba1123ae916c3d1da2922d
Argmaster <argmaster.world@gmail.com>
2021-09-28 00:57:00 +0200

    Added automatic download of missing binaries on wheel release

--- src/PyR3/unpack.py
+++ src/PyR3/unpack.py
@@ -11,6 +11,9 @@ import requests
 IS_LINUX = platform.system() == "Linux"
 IS_WINDOWS = platform.system() == "Windows"
 
+BPY_WIN_DIST_NAME = "bpy_2_93-win_amd64-cp39.tar.gz"
+BPY_LINUX_DIST_NAME = "bpy_2_93-linux_64-cp39.tar.gz"
+
 
 def unpack_lib():
     if check_unpack_required():
@@ -52,9 +55,20 @@ if is_installed():
     def bpy_tar_gz_path() -> Path:
         lib_dir = Path(__file__).parent.parent.parent
         if IS_WINDOWS:
-            return lib_dir / "bpy_2_93-win_amd64-cp39.tar.gz"
+            path = lib_dir / BPY_WIN_DIST_NAME
+            if not os.path.exists(path):
+                download_bpy_binaries(
+                    BPY_WIN_DIST_NAME,
+                    path,
+                )
         elif IS_LINUX:
-            return lib_dir / "bpy_2_93-linux_64-cp39.tar.gz"
+            path = lib_dir / BPY_LINUX_DIST_NAME
+            if not os.path.exists(path):
+                download_bpy_binaries(
+                    BPY_LINUX_DIST_NAME,
+                    path,
+                )
+        return path
 
 
 else:
@@ -62,14 +76,11 @@ else:
     def bpy_tar_gz_path() -> Path:
         if IS_WINDOWS:
             path = (
-                Path(__file__).parent.parent.parent
-                / "lib"
-                / "win"
-                / "bpy_2_93-win_amd64-cp39.tar.gz"
+                Path(__file__).parent.parent.parent / "lib" / "win" / BPY_WIN_DIST_NAME
             )
             if not os.path.exists(path):
-                download_file(
-                    "https://github.com/Argmaster/pyr3/releases/download/bpy-binaries/bpy_2_93-win_amd64-cp39.tar.gz",
+                download_bpy_binaries(
+                    BPY_WIN_DIST_NAME,
                     path,
                 )
         elif IS_LINUX:
@@ -77,17 +88,25 @@ else:
                 Path(__file__).parent.parent.parent
                 / "lib"
                 / "linux"
-                / "bpy_2_93-linux_64-cp39.tar.gz"
+                / BPY_LINUX_DIST_NAME
             )
             if not os.path.exists(path):
-                download_file(
-                    "https://github.com/Argmaster/pyr3/releases/download/bpy-binaries/bpy_2_93-linux_64-cp39.tar.gz",
+                download_bpy_binaries(
+                    BPY_LINUX_DIST_NAME,
                     path,
                 )
         return path
 
 
+def download_bpy_binaries(dist: str, local_path: Path):
+    download_file(
+        f"https://github.com/Argmaster/pyr3/releases/download/bpy-binaries/{dist}",
+        local_path,
+    )
+
+
 def download_file(url: str, local_path: Path):
+    local_path.parent.mkdir(parents=True, exist_ok=True)
     with requests.get(url, stream=True) as r:
         with open(local_path, "wb") as f:
             shutil.copyfileobj(r.raw, f)
