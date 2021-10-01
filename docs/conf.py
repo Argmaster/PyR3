042bdaddb3a01de5267bc7c831a473c1fde03605
Argmaster <argmaster.world@gmail.com>
2021-10-01 10:21:31 +0200

    Bump version: v0.0.0 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = "PyR3"
 year = "2021"
 author = "Krzysztof Wiśniewski"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.0.0"
+version = release = "0.1.0"
 
 pygments_style = "trac"
 templates_path = ["."]
