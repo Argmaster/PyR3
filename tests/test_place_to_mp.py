59ed3e9240d5ebf1e00171c7f5fa8dcc9280e94a
Argmaster <argmaster.world@gmail.com>
2021-11-02 10:28:56 +0100

    Add test for place_to_mp cli

--- tests/test_place_to_mp.py
+++ tests/test_place_to_mp.py
@@ -7,8 +7,17 @@ from unittest import TestCase
 from PyR3.place_to_mp import main
 
 DIR = Path(__file__).parent
+TEMP_DIR = DIR / ".temp"
+TEMP_DIR.mkdir(0o777, True, True)
 
 
 class TestPlace_to_MP(TestCase):
     def test_valid_file_conversion(self):
-        main()
+        main(
+            [
+                "--place",
+                str(DIR / "test_construct" / "LMP.place"),
+                "--mp",
+                str(TEMP_DIR / "test.mp.yaml"),
+            ]
+        )
